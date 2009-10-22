8187b5ee58bf15148e6a3cffca738deb7452dd8b
Matthew Harris <kuja@byteflux.net>
2009-10-22 14:12:25 -0700

    Removing custom record class from model class dependencies.

--- extensions/data/Model.php
+++ extensions/data/Model.php
@@ -6,7 +6,6 @@ class Model extends \lithium\data\Model {
 	
 	protected $_classes = array(
 		'query' => '\li3_doctrine\extensions\data\model\Query',
-		'record' => '\li3_doctrine\extensions\data\model\Record',
 		'recordSet' => '\li3_doctrine\extensions\data\model\RecordSet'
 	);
 	
